5deb1db971fdfc1c05409a649f1dc9d9b849c6e5
Rocío Valedo Santiago <181098@ids.upchiapas.edu.mx>
2020-07-03 16:38:07 -0500

    First commit and MySQL connection

---
+++ src/farmacia_legado/Main.java
@@ -0,0 +1,29 @@
+package farmacia_legado;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Label label =  new Label("Connection to MySQL: "+MySQLConnection.getConnection());
+		Group root = new Group();
+		root.getChildren().addAll(label);
+		label.setLayoutX(120);
+		label.setLayoutY(200);
+        Scene scene = new Scene(root, 640, 400);
+        primaryStage.setScene(scene);
+        primaryStage.getIcons().add(new Image("/assets/images/legado_farmacia.png"));
+        primaryStage.setTitle("Farmacia Legado");
+        primaryStage.show();
+	}
+}
